2b21f04f8ac688dd3b5b53a20cbe272749fc953e
Allen Clarence <zombiedelah@gmail.com>
2022-07-07 23:49:53 +0900

    [Refactor] Add trait bound for new types of node for the menu

--- addressbook/src/app.rs
+++ addressbook/src/app.rs
@@ -1,14 +1,23 @@
-use crate::components::menu::{Entry, Menu};
+use crate::components::contact::{Contact, Profile};
+use crate::components::menu::{Node, Internal, Leaf};
+use std::boxed::Box;
+
+enum Event {
+    New,
+    Open,
+    Create,
+    Read,
+    
+}
 
 pub fn run_app() {
-    let main_menu = Menu::new(vec![
-        ('1'.to_string(), Entry::new("새 주소록 만들기")),
-        ('2'.to_string(), Entry::new("기존 주소록 열기")),
-        ('3'.to_string(), Entry::new("새 주소")),
-        ('4'.to_string(), Entry::new("찾기")),
-        ('5'.to_string(), Entry::new("수정")), 
-        ('6'.to_string(), Entry::new("삭제")),
-        ('`'.to_string(), Entry::new("종료")),
+    let main_menu = Internal::build("Main Menu".to_string(), vec![
+        ('1'.to_string(), Box::new(Internal::new("새 주소록".to_string()))),
+        ('2'.to_string(), Box::new(Internal::new("주소록 열기".to_string()))),
+        ('3'.to_string(), Box::new(Internal::new("프로필 생성".to_string()))),
+        ('4'.to_string(), Box::new(Internal::new("보기".to_string()))),
+        ('6'.to_string(), Box::new(Internal::new("삭제".to_string()))),
+        ('`'.to_string(), Box::new(Internal::new("종료".to_string()))),
     ]);
 
     main_menu.run();
