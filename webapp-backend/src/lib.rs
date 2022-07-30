ba9ddc864c4b790631fa95a9ea59a58d682d69e7
Allen Clarence <zombiedelah@gmail.com>
2022-07-31 08:33:47 +0900

    [Refactor] Change handlers from service => route

--- webapp-backend/src/lib.rs
+++ webapp-backend/src/lib.rs
@@ -1,9 +1,10 @@
 mod login;
 use login::get_oauth_client;
 
+use actix_files::NamedFile;
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder};
-use actix_web::{get, post, web};
 use actix_web::dev::Server;
+use actix_web::web;
 
 use serde::Deserialize;
 
@@ -16,23 +17,27 @@ struct UserInfo {
     prompt: String,
 }
 
-#[get("/auth")]
-async fn authorize_user(user_info: web::Query<UserInfo>) -> impl Responder {
-    dbg!(user_info);
-    HttpResponse::Ok()
+// "/"
+async fn index() -> std::io::Result<NamedFile> {
+    let index_path = std::path::PathBuf::from("/static/index.html");
+    Ok(NamedFile::open(index_path)?)
 }
 
-#[get("/")]
-async fn sign_in() -> impl Responder {
-    HttpResponse::Found()
+// "/auth"
+async fn authorize(user_info: web::Data<UserInfo>) -> impl Responder {
+    println!("{:?}", user_info);
+    HttpResponse::Ok().finish()
 }
 
 pub fn run_app() -> Result<Server, std::io::Error> {
+    println!("Starting the http server at 'http://127.0.0.1:8080'.");
+
     let server = HttpServer::new(|| {
         let oauth_client = web::Data::new(get_oauth_client().unwrap());
         App::new()
             .app_data(oauth_client)
-            .service(authorize_user)
+            .route("/", web::get().to(index))
+            .route("/auth", web::get().to(authorize))
     })
     .bind("127.0.0.1:8080")?
     .run();
